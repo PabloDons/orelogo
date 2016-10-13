d32667c8828350f05aaab8e16700baec3c2c0637
Omar Saad <PabloDons@users.noreply.github.com>
2016-10-13 18:42:20 +0200

    Update orelogo.php

--- orelogo.php
+++ orelogo.php
@@ -1,4 +1,6 @@
 <?php
+//config
+$font_folder = "/user/share/fonts";
 
 header("Content-type: image/png");
 
@@ -10,11 +12,11 @@ isset($_GET['y']) OR $_GET['ms'] ? $_GET['y'] = '10' : $_GET['y'] = '24';
 isset($_GET['b']) OR $_GET['b'] = "0";
 isset($_GET['b']) OR $_GET['i'] = "0";
 
-$_GET['ms'] ? $im = imagecreatefrompng("logo-ms.png"): $im = imagecreatefrompng("logo-opaque.png");
+$_GET['ms'] ? $im = imagecreatefrompng("images/logo-ms.png"): $im = imagecreatefrompng("images/logo-opaque.png");
 $gray   = imagecolorallocate($im, 64, 64, 64);
-$_GET['i'] ? $font = './ariali.ttf' : $font = './arial.ttf';
-$_GET['b'] ? $font = './arialbd.ttf' : false;
-$_GET['i'] AND $_GET['b'] ? $font = './arialbi.ttf' : false;
+$_GET['i'] ? $font = "$font_folder/ariali.ttf" : $font = "$font_folder/arial.ttf";
+$_GET['b'] ? $font = "$font_folder/arialbd.ttf" : false;
+$_GET['i'] AND $_GET['b'] ? $font = "$font_folder/arialbi.ttf" : false;
 
 imagettftext($im, intval($_GET['s']), 0, intval($_GET['x']), imagesy($im) - intval($_GET['y']), $gray, $font, $_GET['d']);
 imagepng($im);
